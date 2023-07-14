78dbc97339f2c881eab34b6ae77704e6a4d3656d
Ikey Doherty <ikey@serpentos.com>
2023-07-14 22:34:16 +0100

    Initial dub skeleton
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/app.d
@@ -0,0 +1,21 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2023 Ikey Doherty
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * main
+ *
+ * Main test app for the API being fleshed out
+ *
+ * Authors: Copyright © 2023 Ikey Doherty
+ * License: Zlib
+ */
+
+module main;
+
+void main()
+{
+    imported!"std.stdio".writeln("TODO: Anything useful");
+}
