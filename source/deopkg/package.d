ead7e44744550a2a2fa942dd4887589e5c1dc725
Ikey Doherty <ikey@serpentos.com>
2023-07-14 22:41:53 +0100

    deopkg: Add the root package
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/deopkg/package.d
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2023 Ikey Doherty
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * deopkg
+ *
+ * Main portions of deopkg
+ *
+ * Authors: Copyright © 2023 Ikey Doherty
+ * License: Zlib
+ */
+
+module deopkg;
