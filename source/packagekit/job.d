434151d187c53a188916a18c52c0ed3cd0ba875c
Ikey Doherty <ikey@serpentos.com>
2023-07-15 23:06:54 +0100

    job: Add some missing function linkage
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/packagekit/job.d
+++ source/packagekit/job.d
@@ -16,7 +16,12 @@
 module packagekit.job;
 
 import ldc.attributes : weak;
+import packagekit.enums : PkInfoEnum, PkStatusEnum, PkErrorEnum;
 
 extern (C) struct PkBackendJob;
 
 @weak extern (C) void pk_backend_job_finished(PkBackendJob* self) @trusted;
+@weak extern (C) void pk_backend_job_package(PkBackendJob* self,
+        PkInfoEnum info, const char* pkgID, const char* summary) @trusted;
+@weak extern (C) void pk_backend_job_set_status(PkBackendJob* job, PkStatusEnum status) @trusted;
+@weak extern (C) void pk_backend_job_error_code(PkBackendJob* job, PkErrorEnum code) @trusted;
